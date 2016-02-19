17cb3f045e6c203dcaffb69d09626bbd4a718008
Spencer <spencer.p.816@gmail.com>
2016-02-19 14:56:11 -0800

    Make Shiny app reactive

--- shiny-example/server.R
+++ shiny-example/server.R
@@ -1,10 +1,12 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  # Create a histogram property of the output
-  output$histogram <- renderPlot({
-    # Use shiny's renderPlot function to render a plot
-    x <- rnorm(100)
-    return(hist(x))
-  })
+   # Create a histogram property of the output
+   output$histogram <- renderPlot({
+      # Use shiny's renderPlot function to render a plot
+      
+      # Get the number of observations from the UI
+      x <- rnorm(input$num)
+      return(hist(x))
+   })
 })
--- shiny-example/ui.R
+++ shiny-example/ui.R
@@ -2,14 +2,17 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(
-  fluidPage(
-    # Application title
-    titlePanel("Hello Shiny!"), 
-    
-    # Create a mainPanel of the app
-    mainPanel(
-      # Plot the output with the name "histogram"
-      plotOutput('histogram')
-    )
-  )
+   fluidPage(
+      # Application title
+      titlePanel("Hello Shiny!"), 
+      
+      # Create a mainPanel of the app
+      mainPanel(
+         # Add a numbericInput to choose the number of observations
+         numericInput("num", label = "# observations", value = 100),
+         
+         # Plot the output with the name "histogram"
+         plotOutput('histogram')
+      )
+   )
 )
